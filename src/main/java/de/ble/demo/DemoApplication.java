d86df6e059e09eeb28715f068639fc932fd7a825
Philipp Hügelmeyer <p.huegelmeyer@gmail.com>
2018-01-20 22:04:52 +0100

    Changed to use spring-cloud-function.

--- src/main/java/de/ble/demo/DemoApplication.java
+++ src/main/java/de/ble/demo/DemoApplication.java
@@ -1,7 +1,12 @@
 package de.ble.demo;
 
+import java.util.function.Function;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import reactor.core.publisher.Flux;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -9,4 +14,10 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
+
+	@Bean
+	public Function<Flux<String>, Flux<String>> uppercase() {
+		return flux -> flux.log().map(value -> value.toUpperCase());
+	}
+
 }
--- src/test/java/de/ble/demo/DemoApplicationTests.java
+++ src/test/java/de/ble/demo/DemoApplicationTests.java
@@ -1,16 +1,33 @@
 package de.ble.demo;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.boot.context.embedded.LocalServerPort;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class DemoApplicationTests {
 
+	@LocalServerPort
+	private int port;
+	
 	@Test
 	public void contextLoads() {
 	}
+	
+	@Test
+	public void uppercase() {
+		// TODO: make this work with a JSON stream as well (like in WebFlux)
+		assertThat(new TestRestTemplate().postForObject(
+				"http://localhost:" + port + "/uppercase", "test",
+				String.class)).isEqualTo("TEST");
+	}
+
 
 }
