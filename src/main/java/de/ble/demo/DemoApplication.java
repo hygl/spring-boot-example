fc53190e2d2b1bd47f8c6acbdec5097a52a7ccac
Philipp Hügelmeyer <p.huegelmeyer@gmail.com>
2018-02-03 15:44:50 +0100

    some more tweaks for a servlet container

--- src/main/java/de/ble/demo/DemoApplication.java
+++ src/main/java/de/ble/demo/DemoApplication.java
@@ -4,12 +4,14 @@ import java.util.function.Function;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 
 import reactor.core.publisher.Flux;
 
 @SpringBootApplication
-public class DemoApplication {
+public class DemoApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
@@ -20,4 +22,11 @@ public class DemoApplication {
 		return flux -> flux.log().map(value -> value.toUpperCase());
 	}
 
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+			return application.sources(applicationClass);
+	}
+
+	private static Class<DemoApplication> applicationClass = DemoApplication.class;
 }
