8328d7c83ce602b7c50c69f9c5e4872969fd3b45
Philipp Hügelmeyer <hygl@github.io>
2018-07-01 15:38:26 +0200

    Test gefixed

--- src/test/java/de/ble/demo/DemoApplicationTests.java
+++ src/test/java/de/ble/demo/DemoApplicationTests.java
@@ -1,32 +1,16 @@
 package de.ble.demo;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.context.embedded.LocalServerPort;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
-import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@SpringBootTest
 public class DemoApplicationTests {
-
-	@LocalServerPort
-	private int port;
 	
 	@Test
 	public void contextLoads() {
 	}
-	
-	@Test
-	public void uppercase() {
-			assertThat(new TestRestTemplate().postForObject(
-				"http://localhost:" + port + "/uppercase", "test",
-				String.class)).isEqualTo("TEST");
-	}
-
 
 }
