7f1e52bcb267b85047b45df93bb71e717d59f49b
Philipp Hügelmeyer <p.huegelmeyer@gmail.com>
2020-09-28 12:13:24 +0200

    Fixed Demo App

--- src/main/java/de/ble/demo/todo/TodoController.java
+++ src/main/java/de/ble/demo/todo/TodoController.java
@@ -1,38 +1,37 @@
 package de.ble.demo.todo;
 
-import java.util.Date;
+
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class TodoController{
 
-  private TodoRepository todoRepository;
+  private final TodoRepository todoRepository;
 
   @Autowired
-  public TodoController(TodoRepository todoRepository){
-    this.todoRepository = todoRepository; 
+  public TodoController(final TodoRepository todoRepository) {
+    this.todoRepository = todoRepository;
   }
-
 
   @PostMapping("/todo")
-  public String addTodo(@RequestParam String task,@RequestParam(required = false) Date datum) {
-	  if (datum !=null) {
-		  todoRepository.save(new Todo(task, datum));
-	  }else {
-		  todoRepository.save(new Todo(task, null));
-	  }
-	  return "redirect:/todos";
+  public Todo addTodo(@RequestBody final Todo newTodo) {
+    return todoRepository.save(newTodo);
   }
-  
+
   @GetMapping("/todos")
-  public String getTodos(Model model){
-    model.addAttribute("todos", todoRepository.findAll());
-    return "todos";
+  public Iterable<Todo> getTodos() {
+    return todoRepository.findAll();
+  }
+
+  @GetMapping("/todo/{id}")
+  public Optional<Todo> getTodo(@PathVariable("id") final long id) {
+    return todoRepository.findById(id);
   }
 }
--- src/test/java/de/ble/demo/HttpRequestTest.java
+++ src/test/java/de/ble/demo/HttpRequestTest.java
@@ -1,30 +1,27 @@
 package de.ble.demo;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
 
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
-@RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@SpringBootTest
+@AutoConfigureMockMvc
 public class HttpRequestTest {
 
-    @LocalServerPort
-    private int port;
-
     @Autowired
-    private TestRestTemplate restTemplate;
+	private MockMvc mockMvc;
+
 
     @Test
     public void greetingShouldReturnDefaultMessage() throws Exception {
-        assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/hello",
-                String.class)).contains("Hello");
+        this.mockMvc.perform(get("/hello")).andDo(print()).andExpect(status().isOk());//.andExpect(content("Hello"));
     }
 }
