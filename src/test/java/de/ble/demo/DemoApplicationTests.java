7ddf96464e586910926ed2bc91ac3240c467e36e
Philipp Hügelmeyer <p.huegelmeyer@gmail.com>
2020-10-19 15:31:47 +0200

    Fiexed test and pipeline

--- src/test/java/de/ble/demo/DemoApplicationTests.java
+++ src/test/java/de/ble/demo/DemoApplicationTests.java
@@ -5,16 +5,18 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import de.ble.demo.todo.TodoController;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 public class DemoApplicationTests {
 	
 	@Autowired
-	private HelloController helloController;
+	private TodoController todoController;
 	@Test
 	public void contextLoads() {
-		assertThat(helloController).isNotNull();
+		assertThat(todoController).isNotNull();
 	}
 
 }
